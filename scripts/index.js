47dd1022512b3eff9491f6a4f7ee74a20418cbff
CaiphasCain <finthehuma@yandex.ru>
2022-01-22 20:48:52 +0300

    hotfix/firstReview

--- scripts/index.js
+++ scripts/index.js
@@ -3,40 +3,38 @@ let popup = document.querySelector('.popup');
 let popupSubmitButton = document.querySelector('.popup__submit-button');
 let popupCloseButton = document.querySelector('.popup__close-button');
 let formElement = document.querySelector('.popup__form')
-let nameInput = document.querySelector('.popup__form-name')
-let jobInput = document.querySelector('.popup__form-desc')
+let nameInput = document.querySelector('#popup__form-name')
+let jobInput = document.querySelector('#popup__form-desc')
 let profileName = document.querySelector('.profile__name')
 let profileDesc = document.querySelector('.profile__desc')
-let elements = document.querySelector('.elements')
-let likeButtons = elements.querySelectorAll('.element__like')
+// let element = document.querySelector('.elements')
+// let likeButtons = element.querySelectorAll('.element__like')
 
-function editButtonClick() {
-  popup.classList.add('popup_opened')
+function onEditButtonClick() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileDesc.textContent;
+  popup.classList.add('popup_opened')
 };
-profileEditButton.addEventListener('click', editButtonClick);
 
-function closeButtonClick() {
+function onCloseButtonClick() {
   popup.classList.remove('popup_opened');
 };
-popupCloseButton.addEventListener('click', closeButtonClick);
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  let nameValue = nameInput.value;
-  let jobValue = jobInput.value;
-  profileName.textContent = nameValue;
-  profileDesc.textContent = jobValue;
+  profileName.textContent = nameInput.value;
+  profileDesc.textContent = jobInput.value;
   popup.classList.remove('popup_opened');
 };
 
+// for (let i = 0; i < likeButtons.length; i++){
+//   likeButtons[i].addEventListener('click', likeActive)
+// }
+
+// function likeActive(){
+//   this.classList.toggle('element__like_active');
+// };
+
+profileEditButton.addEventListener('click', onEditButtonClick);
+popupCloseButton.addEventListener('click', onCloseButtonClick);
 formElement.addEventListener('submit', formSubmitHandler);
-
-for (let i = 0; i < likeButtons.length; i++){
-  likeButtons[i].addEventListener('click', likeActive)
-}
-
-function likeActive(){
-  this.classList.toggle('element__like_active');
-};
